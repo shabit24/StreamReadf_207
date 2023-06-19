1e12a4207c2036d36077bca96e0c4cd49507d952
shabit24 <hossain.shabit24@gmail.com>
2023-06-19 16:40:20 +0700

    finish

--- file1.cpp
+++ file1.cpp
@@ -1,20 +1,36 @@
-// file1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <fstream>
 #include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+	string baris;
+	ofstream outfile;
+	outfile.open("namafile.txt");
+	cout << ">= Menulis file, \'q\' untuk kelur" << endl;
+
+
+	while (true) {
+		cout << "- ";
+		getline(cin, baris);
+
+		if (baris == "q") break;
+		outfile << baris << endl;
+
+
+	}
+	outfile.close();
+	ifstream infile;
+	infile.open("namafile.txt");
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	if (infile.is_open()) {
+		while (getline(infile, baris)) {
+			cout << baris << '\n';
+		}
+		infile.close();
+	}
+	else cout << "Unable to open file";
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
